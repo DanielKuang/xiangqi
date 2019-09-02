85c383d2cbec7942eb8578476d8720483c1cdcb3
DanielKuang <dk6548@mit.edu>
2019-09-02 00:15:36 -0400

    updated css file
    - updated tile in component folder
    - added css

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import '../index.css'
 
-export default function Tile(props){
-    return (<button onClick = {props.onClick}></button>)
+export default function Tile(props) {
+    return (
+        <button className = {"tile"  + props.shade}
+        onClick = {props.onClick}
+        style = {props.style}>
+        </button>
+    );
 }
